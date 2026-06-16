5ede8b09409fb24117c6c931ecdafbd9a18ea180
harmain27-04 <fathimaharmain95@gmail.com>
2026-06-16 15:45:03 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "usn-1BI23CS083\n Name-HARMAIN" );
+    }
+}
